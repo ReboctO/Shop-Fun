f94bc367bedbae106f3ee8cc54c7f584bba9402f
erllocaylocay <117888162+erllocaylocay@users.noreply.github.com>
2024-05-06 08:51:05 +0800

    first commit

---
+++ ShopFun/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace ShopFun
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Models.Items> itemsList = new List<Models.Items>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            populateItems();
+
+        }
+        public void populateItems()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+                Models.Items itemsControl = new Models.Items();
+                itemsControl.BrandName = "Lacoste Bag ";
+                itemsControl.BrandPrice = 100 * (i + 1);
+                itemsList.Add(itemsControl);
+            }
+            foreach (var item in itemsList)
+            {
+                stackItems.Children.Add(item);
+            }
+        }
+        private void userButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ShopFun/Models/Items.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ShopFun.Models
+{
+    /// <summary>
+    /// Interaction logic for Items.xaml
+    /// </summary>
+    public partial class Items : UserControl
+    {
+        public Image _img;
+        public string _brandName;
+        public double _brandPrice;
+        public Items()
+        {
+            InitializeComponent();
+        }
+
+        public Image Image
+        {
+            get { return _img; }
+            set { _img = value; itemImage = value; }
+        }
+        public string BrandName
+        {
+            get { return _brandName; }
+            set { _brandName = value; itemName.Content = value; }
+        }
+        public double BrandPrice
+        {
+            get { return _brandPrice; }
+            set { _brandPrice = value; itemPrice.Content = "$ " + value; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
